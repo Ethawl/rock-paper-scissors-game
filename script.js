05f63d9bee5931b6f6e1010ef23420eaed37eb3e
Denns <dennis.orozco.jr1@gmail.com>
2024-06-05 07:08:04 +0800

    100% done

--- script.js
+++ script.js
@@ -12,49 +12,97 @@ function getComputerChoice() {
 }
 
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Make a move between these tree: Rock, Paper, or Scissors", '');
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-
+function Result() {
+    // if (playerScore > computerScore) {
+    //     console.log(`Final Score
+    //       \nPlayer Score: ${playerScore}
+    //       \nComputer Score : ${computerScore}
+    //       \nThe player wins the game`);
+    // } else {
+    //     console.log(`Final Score
+    //       \nPlayer Score: ${playerScore}
+    //       \nComputer Score : ${computerScore}
+    //       \nThe computer wins the game`);
+    // }
     if (playerScore > computerScore) {
-        console.log(`Final Score
-      \nPlayer Score: ${playerScore}
-      \nComputer Score : ${computerScore}
-      \nThe player wins the game`);
-
-    } else if (computerScore > playerScore) {
-        console.log(`Final Score
-      \nPlayer Score: ${playerScore}
-      \nComputer Score : ${computerScore}
-      \nThe computer wins the game`);
+        p1.textContent = `Final Score`;
+        p2.textContent = `Player Score: ${playerScore}`;
+        p3.textContent = `Computer Score : ${computerScore}`;
+        p4.textContent = `The player wins the game`;
     } else {
-        console.log('It is a tie');
+        p1.textContent = `Final Score`;
+        p2.textContent = `Player Score: ${playerScore}`;
+        p3.textContent = `Computer Score : ${computerScore}`;
+        p4.textContent = `The computer wins the game`;
     }
-
-
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        return 'It is a tie';
+function disableBtn() {
+    RockBtn.disable = true;
+    PaperBtn.disable = true;
+    ScissorsBtn.disable = true;
+}
+
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    if (playerScore === 5 || computerScore === 5) {
+        p4.textContent = '';
+        Result();
+        disableBtn();
+        return;
     }
-    else if
-        ((playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors') ||
-        (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') ||
-        (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock')
-    ) {
-        playerScore++;
-        console.log(`Player Score: ${playerScore}\n Computer Score: ${computerScore}`);
-        return `You win! ${playerSelection} beats ${computerSelection}`
+
+    if (playerSelection === computerSelection) {
+        p2.textContent = `Player Score: ${playerScore}`;
+        p3.textContent = `Computer Score: ${computerScore}`;
+        p4.textContent = 'It is a tie'
     }
     else {
-        computerScore++;
-        console.log(`Player Score: ${playerScore}\n Computer Score: ${computerScore}`);
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+        if (
+            (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+            (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+            (playerSelection === 'Scissors' && computerSelection === 'Paper')
+        ) {
+            playerScore++;
+            p2.textContent = `Player Score: ${playerScore}`;
+            p3.textContent = `Computer Score: ${computerScore}`;
+            p4.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+
+        } else {
+            computerScore++;
+            p2.textContent = `Player Score: ${playerScore}`;
+            p3.textContent = `Computer Score: ${computerScore}`;
+            p4.textContent = `You lose! ${playerSelection} beats ${computerSelection}`;
+        }
+
+        if (playerScore === 5 || computerScore === 5) {
+            p4.textContent = '';
+            Result();
+            disableBtn();
+        }
+
     }
 }
 
-playGame()
+const RockBtn = document.querySelector("#Rock");
+const ScissorsBtn = document.querySelector("#Scissors");
+const PaperBtn = document.querySelector("#Paper");
+const DivContainer = document.querySelector(".container");
+const p1 = document.querySelector("#p1");
+const p2 = document.querySelector("#p2");
+const p3 = document.querySelector("#p3");
+const p4 = document.querySelector("#p4");
+
+
+RockBtn.addEventListener("click", () => {
+    playRound("Rock");
+});
+
+PaperBtn.addEventListener("click", () => {
+    playRound("Paper");
+});
+
+ScissorsBtn.addEventListener("click", () => {
+    playRound("Scissors");
+});
+
